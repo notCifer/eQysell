ca0fde2eb88c87a71792fa734c753cf8e4c63112
Erika Keisy <erika.santos_BOLS@compasso.com.br>
2021-06-16 13:22:57 -0300

    Add Email e Senha Usuario

--- src/main/java/com/projeto/app/models/Usuario.java
+++ src/main/java/com/projeto/app/models/Usuario.java
@@ -3,13 +3,36 @@ package com.projeto.app.models;
 public class Usuario {
 
     private Long id;
-    
+    private String email;
+
+    public Usuario() {
+    }
+
+    private String senha;
+
     public Long getId() {
         return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
 
 }
