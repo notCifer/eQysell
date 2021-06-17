13a1b384fb6fd84bd757e0e077812da73f908e59
Allan Patrick <Allan.15.patrick@gmail.com>
2021-06-17 09:59:37 -0300

    Adicionando Form e Dto

--- src/main/java/com/projeto/app/repositories/UsuarioRepository.java
+++ src/main/java/com/projeto/app/repositories/UsuarioRepository.java
@@ -3,6 +3,6 @@ package com.projeto.app.repositories;
 import com.projeto.app.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
 }
