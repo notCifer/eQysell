3943c7e68a5dd9be18f9f7ce3b34d2d060693002
Allan Patrick <Allan.15.patrick@gmail.com>
2021-06-17 11:56:06 -0300

    Designer de Codigos

--- src/main/java/com/projeto/app/models/dto/LocatarioDTO.java
+++ src/main/java/com/projeto/app/models/dto/LocatarioDTO.java
@@ -7,52 +7,47 @@ import com.projeto.app.models.Locatario;
 
 public class LocatarioDTO {
 
-    // INFORMAÇÕES DTO
-    
     private String nome;
     private String email;
     private Long telefone;
 
-     // GET E SET 
+    /* ___________________________GETTERS and SETTERS___________________________ */
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public Long getTelefone() {
         return telefone;
     }
+
     public void setTelefone(Long telefone) {
         this.telefone = telefone;
     }
 
-    public LocatarioDTO toDTO(Locatario locatario){
-       LocatarioDTO DTO = new LocatarioDTO();
-       DTO.setNome(locatario.getNome());
-       DTO.setEmail(locatario.getEmail());
-       DTO.setTelefone(locatario.getTelefone());
-       return DTO;
+    /* ___________________________METODO toDTO___________________________ */
+
+    public LocatarioDTO toDTO(Locatario locatario) {
+        LocatarioDTO DTO = new LocatarioDTO();
+        DTO.setNome(locatario.getNome());
+        DTO.setEmail(locatario.getEmail());
+        DTO.setTelefone(locatario.getTelefone());
+        return DTO;
     }
 
-    public List<LocatarioDTO> toDTO(List<Locatario> locatarios){
+    public List<LocatarioDTO> toDTO(List<Locatario> locatarios) {
         return locatarios.stream().map(locatario -> toDTO(locatario)).collect(Collectors.toList());
     }
 
-  
-
-   
-
-   
-
-    
-
-
-    
 }
--- src/main/java/com/projeto/app/models/form/LocatarioFORM.java
+++ src/main/java/com/projeto/app/models/form/LocatarioFORM.java
@@ -9,37 +9,35 @@ import com.projeto.app.repositories.LocatarioRepository;
 
 public class LocatarioFORM {
 
-    // INFORMAÇÕES PESSOAIS
-    @NotBlank @NotNull
+    @NotBlank
+    @NotNull
     private String nome;
     @NotNull
     @Size(max = 11)
     private Long cpf;
-    @NotBlank @NotNull
+    @NotBlank
+    @NotNull
     private String endereco;
-    @NotBlank @NotNull
+    @NotBlank
+    @NotNull
     private String email;
     @NotNull
-    @Size (max = 12)
+    @Size(max = 12)
     private Long telefone;
 
-
-    // GET E SET
+    /* ___________________________GETTERS and SETTERS___________________________ */
     public String getNome() {
         return nome;
     }
 
-    
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    
     public Long getCpf() {
         return cpf;
     }
 
-   
     public void setCpf(Long cpf) {
         this.cpf = cpf;
     }
@@ -48,35 +46,30 @@ public class LocatarioFORM {
         return endereco;
     }
 
-    
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 
-    
     public String getEmail() {
         return email;
     }
 
-  
     public void setEmail(String email) {
         this.email = email;
     }
 
-    
     public Long getTelefone() {
         return telefone;
     }
 
-   
     public void setTelefone(Long telefone) {
         this.telefone = telefone;
     }
 
-    //
+    /* ___________________________toFORM___________________________ */
 
-    public Locatario toFORM (LocatarioRepository LocatarioR){
-        Locatario locatario = new Locatario(nome,cpf,endereco,email,telefone);
+    public Locatario toFORM(LocatarioRepository LocatarioR) {
+        Locatario locatario = new Locatario(nome, cpf, endereco, email, telefone);
         LocatarioR.save(locatario);
         return locatario;
     }
