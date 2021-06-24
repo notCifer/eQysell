03ddcc593954d4cada77e2ac775c7ea900d65aa3
Erika Keisy <erika.santos_BOLS@compasso.com.br>
2021-06-24 09:03:57 -0300

    Relatorio Model

---
+++ src/main/java/com/projeto/app/repositories/RelatorioRepository.java
@@ -0,0 +1,11 @@
+package com.projeto.app.repositories;
+
+import com.projeto.app.models.Relatorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RelatorioRepository extends JpaRepository<Relatorio, Long> {
+    
+}
