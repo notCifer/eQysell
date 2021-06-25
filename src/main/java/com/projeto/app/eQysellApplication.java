e1632da3ca70e271813fcca9f123f1f170334676
Erika Keisy <erika.santos_BOLS@compasso.com.br>
2021-06-25 10:40:32 -0300

    Email

--- src/main/java/com/projeto/app/eQysellApplication.java
+++ src/main/java/com/projeto/app/eQysellApplication.java
@@ -1,13 +1,28 @@
 package com.projeto.app;
 
+import com.projeto.app.controllers.EmailController;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class eQysellApplication {
 
+@Autowired
+private EmailController service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(eQysellApplication.class, args);
+	}
+
+	/* __________________Teste Email_________________ */
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void email(){
+		service.enviarEmail("eqysselproj@gmail.com", "Teste...","teste.");
 	}
 
 	//############## Teste PJ ###############//
